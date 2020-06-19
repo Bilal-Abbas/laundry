7050b170c9462479bd43ff218d21b3d79ff2df20
bilal abbas <bilalabbas437@gmail.com>
2020-06-19 20:44:15 +0500

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery-2.2.3.min
+//= require jquery-ui
+//= require jquery.adipoli.min
+//= require jquery.swipebox.min
+//= require counter
+//= require easing
+//= require move-top
+//= require responsiveslides.min
+//= require SmoothScroll.min
+//= require_tree .
+//= require bootstrap
+//= require owl.carousel
+//= require custom
+
+
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function ($) {
+  $("#slider3").responsiveSlides({
+    auto: true,
+    pager: true,
+    nav: false,
+    speed: 500,
+    namespace: "callbacks",
+    before: function () {
+      $('.events').append("<li>before event fired.</li>");
+    },
+    after: function () {
+      $('.events').append("<li>after event fired.</li>");
+    }
+  });
+});
+
+
+jQuery(document).ready(function ($) {
+  $(".scroll ").click(function (event) {
+    event.preventDefault();
+
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000);
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $().UItoTop({
+    easingType: 'easeOutQuart'
+  });
+})
+
+jQuery(function ($) {
+  $(".swipebox").swipebox();
+});
+
+jQuery(function ($) {
+  $('.row2').adipoli({
+    'startEffect': 'overlay',
+    'hoverEffect': 'sliceDown'
+  });
+});
+
+jQuery(function ($) {
+  $("#datepicker1,#datepicker2").datepicker();
+});
+
+jQuery(document).ready(function ($) {
+  $('#owl-demo').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    autoPlay: false,
+    navigation: true,
+    navigationText: true,
+    pagination: true,
+  });
+})

535ebd80239dfa8d59d34fba7b3eab81951667b6
bilal abbas <bilalabbas437@gmail.com>
2020-08-09 14:42:36 +0500

    added service routes

---
+++ app/controllers/service_controller.rb
@@ -0,0 +1,27 @@
+class ServiceController < ApplicationController
+
+  def shirts
+  end
+
+  def dry_cleaning
+  end
+
+  def household_linen
+  end
+
+  def repairs_alterations
+  end
+
+  def wedding_dress_cleaning
+  end
+
+  def leather_suede
+  end
+
+  def laundry_service
+  end
+
+  def collection_delivery
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'service/shirts'
+  get 'service/dry_cleaning'
+  get 'service/household_linen'
+  get 'service/repairs_alterations'
+  get 'service/wedding_dress_cleaning'
+  get 'service/leather_suede'
+  get 'service/laundry_service'
+  get 'service/collection_delivery'
   root to: 'home#index'
   get "/pricing" => 'home#pricing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

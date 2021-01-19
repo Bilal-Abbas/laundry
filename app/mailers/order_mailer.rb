bbb4fbb31041789470c4f11afc1ad46b35a8d305
Bilal Abbas <bilal.abbas@gigalabs.co>
2021-01-20 02:02:13 +0500

    changed email to quickslivers original email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
 
   def send_email_to_owner params
     @resources = params
-    mail(to: "bilalabbas437@gmail.com", subject: "Order Placed at Quick Silver Dry Cleaner")
+    mail(to: "quicksilverdrycleaners786@gmail.com", subject: "Order Placed at Quick Silver Dry Cleaner")
   end
 end

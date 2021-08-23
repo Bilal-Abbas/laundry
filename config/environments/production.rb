b2fb102bdc9f29d3a19b81ca97804cee702b52b5
Bilal Abbas <bilal.abbas@gigalabs.co>
2021-08-23 13:04:12 +0500

    updated email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,11 +97,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'quicksilverdrycleaner.co.uk' }
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'professionalquicksilverdry@gmail.com',
-    :password             => 'quicksilvers786..',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'professionalquicksilverdry@gmail.com',
+    password:             'quicksilvers786..',
+    authentication:       'plain'
+    # :address              => "smtp.gmail.com",
+    # :port                 => 587,
+    # :user_name            => 'professionalquicksilverdry@gmail.com',
+    # :password             => 'quicksilvers786..',
+    # :authentication       => "plain",
+    # :enable_starttls_auto => true
   }
 end
